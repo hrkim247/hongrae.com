7f0646a73e14fd0fe705e54890ae296ca514a2e4
hrkim247 <hrkim247@gmail.com>
2017-05-20 20:25:35 +0900

    Add source about login

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, make_response
 from apps.database import db_session
+from apps.models import User
 
 app = Flask(__name__)
+
+userDatabase = list()
+
+for ud in User.query.all():
+    userDatabase.append(str(ud))
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
@@ -14,3 +20,13 @@ def index():
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+@app.route("/trylogin")
+def trylogin():
+    id = request.args.get('id')
+    pw = request.args.get('pw')
+
+    if "<{0}, {1}>".format(id, pw) in userDatabase:
+        return jsonify(result="true")
+    else:
+        return jsonify(result="false")
