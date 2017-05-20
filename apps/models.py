767b67c116ffdfaa9caff14315fe3c09a4092b26
hrkim247 <hrkim247@gmail.com>
2017-05-20 13:25:10 +0900

    Create 'users' database code

---
+++ apps/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String
+from apps.database import Base
+
+class User(Base):
+
+    __tablename__ = "users"
+    
+    num = Column(Integer, primary_key=True)
+    id = Column(String, unique=True)
+    pw = Column(String, unique=True)
+
+    def __init__(self, id, pw):
+        self.id = id
+        self.pw = pw
+
+    def __repr__(self):
+        return "<{0}, {1}>".format(self.id, self.pw)
